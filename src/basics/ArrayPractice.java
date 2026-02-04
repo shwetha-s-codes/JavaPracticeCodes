7c409cfd6cde46fff491e59172f2f0c82a7cc265
shwetha-s-codes <shwethakodimar29@gmail.com>
2026-02-04 14:38:43 +0530

    Array Program

---
+++ src/basics/ArrayPractice.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Arrays;
+
+public class ArrayPractice {
+    //Creating array
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        //initializing values
+        arr[0]=1;
+        //arr name is pointing to the address
+        System.out.println(arr);
+        //usage of toString to access elements
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+
+}
